9c1beff4b031d792d1dd612cc1b119cf81c5eb7e
github-personal <saqlaina527@gmail.com>
2023-11-11 15:21:50 +0500

    second commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { CommitsModule } from './commits/commits.module';
+import { PostsModule } from './posts/posts.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, PostsModule, CommitsModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
